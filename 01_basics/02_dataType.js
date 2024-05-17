7517c49abb17fcf4e05556d36bad228d4ae53b68
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2024-05-18 00:09:33 +0530

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) // we are not using nodejs, not browser
+
+//  console.log(3+3);  console.log("Deepak"); 
+
+console.log(3 + 3); 
+// code readbility should be high
+console.log("Deepak");  
+ 
+// DataType 
+
+let name = "Deepak" //string 
+let age = 18 // number 
+let isLoggedIn = false  
+let state ;
+ 
+// premitive Data type :- 
+
+// number => 2 to Power 53 
+// bigint (use like stock market) 
+// string =>
+// boolean => true/false (use like credit card detials he ya nahi he)
+// null => standalone value  (representation of null values )
+// undefined => 
+// symbol => unique  
+
+//object  
+
+console.log(typeof "Deepak"); //variable type batata he 
+console.log(typeof age); 
+console.log(typeof null);  // null ek object he 
+console.log(typeof undefined);
