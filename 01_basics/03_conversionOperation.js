f6d13c664d2d0dfa5e1d66f132065c770cd72fe4
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2024-05-19 01:53:04 +0530

    Conversions and Operations Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -84,7 +84,7 @@ num1 = num2 = num3 = 2 + 2;
 let gameCounter = 100 
 gameCounter++;  // ++gameCounter (incement topic )
 console.log(gameCounter); 
-console.log(typeof gameCounter);
+//console.log(typeof gameCounter);
  
 // link to study  
 
