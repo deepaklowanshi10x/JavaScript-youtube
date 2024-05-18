08db4f719c6b450165bf4e9db10aa2649dea2c68
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2024-05-19 01:08:28 +0530

    Conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+
+/*
+let score = 33 
+let score = "33" 
+let score = null
+let score = undefined
+let score = true
+
+
+
+// data type pata karne ke liye  
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+ 
+
+
+//"33"=> 33
+// "33abc"=> NaN 
+// true => 1; false =>0  
+
+
+*/ 
+
+// let isLoggedIn = 1 
+let isLoggedIn = ""
+// let isLoggedIn = "Deepak"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+// 1 => true; 0=> false 
+// " " =>false 
+// "Deepak" => true 
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+console.log(typeof stringNumber);
