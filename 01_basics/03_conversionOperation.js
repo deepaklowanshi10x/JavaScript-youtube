2bcd482051aa931c91b9940a7f4c5fae88cf704a
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2025-10-25 23:11:28 +0530

    Conversion learn

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1 / false => 0
+// Null => 0
+
+let isLoggedIn = 1 //"","Deepak"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true / 0 => false 
+// " " => false 
+// "Deepak" => true
+
+
+// Convert number to String 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+console.log(typeof stringNumber);
+
+
+
