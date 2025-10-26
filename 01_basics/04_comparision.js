13c0b38b0e7655f2b88abe8766bfc36401c26cc2
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2025-10-26 17:04:04 +0530

    Today Learn Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,38 @@
+// Basic Conversion And avoid this type comparision
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02" > 1);  
+
+// This Type Comparision Avoid 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// Also Avoid 
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0); 
+
+// === Stricly Check with datatype
+// == Equality Check 
+// >< >= <= Comparision
+
+console.log( "2" === 2);
+
+
+
+
+
+
+
+
+
+
