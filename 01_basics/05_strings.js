1068e830ca41c602a14e353b23cc7d1f4a845611
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2025-11-01 01:26:09 +0530

    today I learn String in javaScript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Deepak"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+// this is right syntax but this type not used this time
+
+// console.log('Hello my name is ${name} and my repo count ${repoCount}');
+// moden way syntax called string interpulation
+
+const gameName = new String ("DeepakLowanshi")
+
+console.log(gameName); 
+console.log(gameName[0]); // key value pair Acess
+console.log(gameName.__proto__); //ACESS METHOS
+
+console.log(gameName.length); // length of cherecter
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());  
+
+
+console.log(gameName.charAt(2)); // character position janne ke liye
+console.log(gameName.indexOf('i')); // character positiom
+
+
+
+
+
+
+
+
+
+
+
