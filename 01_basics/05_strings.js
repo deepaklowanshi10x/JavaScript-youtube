e582fff9a0631d15db44f9d0ff4e7b659fefdca9
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2025-11-08 00:59:42 +0530

    Learn String and Number

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,19 +7,40 @@ const repoCount = 50
 // console.log('Hello my name is ${name} and my repo count ${repoCount}');
 // moden way syntax called string interpulation
 
-const gameName = new String ("DeepakLowanshi")
+const gameName = "Deepak-lowanshi"
+console.log(gameName.length); 
+// console.log(gameName[0]); // key value pair Acess
+// console.log(gameName.__proto__); //ACESS METHOS
 
-console.log(gameName); 
-console.log(gameName[0]); // key value pair Acess
-console.log(gameName.__proto__); //ACESS METHOS
-
-console.log(gameName.length); // length of cherecter
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());  
+// console.log(gameName.length); // length of cherecter
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());  
 
 
-console.log(gameName.charAt(2)); // character position janne ke liye
-console.log(gameName.indexOf('i')); // character position
+// console.log(gameName.charAt(2)); // character position janne ke liye
+// console.log(gameName.indexOf('i')); // character position
+
+// new methods more
+
+const newString = gameName.substring(0,6) 
+// console.log(newString);
+
+const anotherString = gameName.slice(-2,14)
+console.log(anotherString); 
+
+// trim method in javaScript
+const newStringOne = '   Deepak   ';
+console.log(newStringOne);
+console.log(newStringOne.trim());
+// remove the extra spaces
+
+// repalce
+const url = "https//Deepak.com/deepak%20lowanshi";
+console.log(url.replace('%20', '-'))
+console.log(url.includes('Deepak')) // true 
+console.log(url.includes('sundar')) // false 
+
+console.log(gameName.split('-'))
 
 
 
---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,16 @@
+const score = 300
+console.log(score);
+
+const balance = new Number(200);
+console.log(balance);
+
+// Number Methods
+console.log(balance.toString()) // 200 in string 
+console.log(balance.toFixed(2)); // value in point in e-commerce web
+
+const otherNumber = 123.899
+console.log(otherNumber.toPrecision(3)); // lagbhag
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')) // easy counting 
+
