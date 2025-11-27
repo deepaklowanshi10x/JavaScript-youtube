69da97ed413be75b2f3467fcf0b684b9d00dd7e0
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2025-11-27 22:25:30 +0530

    Date in Js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString()); // Thu Nov 27 2025 14;29:29 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString()); //Thu Nov 27 2025
+console.log(myDate.toISOString()); // 2025-11-27T09:02:53.817Z
+console.log(myDate.toLocaleString()); //  11/27/2025, 2:34:33 PM 
+console.log(myDate.toLocaleTimeString()); // 9:34:28 PM
