21fb5df4091ba28a03156bc2613471ec585fac7d
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-01-14 16:54:10 +0530

    JSON talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,13 +21,15 @@ const regularUser ={
 
 // console.log(regularUser.fullname.userfullname.firstname); // Acess karne ka tarika
 
+// combining objects
+
  const obj1 = { 1: "one", 2: "two"}
  const obj2 = { 3: "three", 4: "four"} 
 
 //  const obj3 = {obj1, obj2}
 
 //  console.log(obj3); // bahi error object ke andar object ban gaya he
-
+// object.assign() se kar sakte he
 //  const obj3store = Object.assign({}, obj1, obj2); // empty object dene achhi baat he 
     const obj3store = {...obj1, ...obj2}; // spread operator se bhi kar sakte he
 //  console.log(obj3store); // ab ye sahi ho gaya he   
@@ -66,3 +68,35 @@ console.log(Object.entries(tinderuser));
 console.log(tinderuser.hasOwnProperty("name")); // true
 console.log(tinderuser.hasOwnProperty("country")); // false
 
+// ************** object part 3 - methods **************
+
+// object ko destructuring karna
+const course ={
+    coursename : "js basics",
+    price : 999,
+    courseInstructor : "Deepak Lowanshi",   
+}  
+
+// course.courseInstructor // normal way to access
+
+// destructuring karna
+const {courseInstructor} = course;
+console.log(courseInstructor); // Deepak Lowanshi
+
+// destructuring with rename
+const {price : coursePrice} = course;
+console.log(coursePrice); // 999    
+
+
+// API se data aata he to wo JSON format me aata he  
+// JSON object format me kuch is tarah hota he
+// {
+//     coursename : "js basics",
+//     price : 999,
+//     courseInstructor : "Deepak Lowanshi",   
+// } 
+
+[
+{},
+{},
+]
