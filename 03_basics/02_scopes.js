e68c7940a5f36676813ae5249314fd124ee80f8e
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-01-15 19:34:15 +0530

    scope in functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -20,9 +20,56 @@ let a =200
 if(true){
     let a = 10
     const b = 20           // block scope    
-    console.log("INNER:", a) // inner me a ki value 10 he 
+    // console.log("INNER:", a) // inner me a ki value 10 he 
 }
  
-console.log(a); // Globle scope ki value dega agar hum block scope ke andar console log kare to andar ki value dega 
+// console.log(a); // Globle scope ki value dega agar hum block scope ke andar console log kare to andar ki value dega 
+
+ // ******* Nested scope******
+
+ //example  
+ // Child function perent function ko acess kar sakti he
+ function one() {
+    const username  = "Deepak"
+
+    function two(){
+        const website =" youtube"    
+        console.log(username); //Deepak 
+    } 
+    // console.log(website); // error
+    two()
+
+ } 
+ one()  
+
+ // Example 2 if elses me
+
+ if(true) {
+    const username ="deepak"
+
+    if(username === "deepak"){
+        const website = "youtube"
+        console.log(username + website); // deepakyoutube
+    } 
+
+    // console.log(website); //error
+ } 
+
+//  console.log(username); // error username is not defined
+
+//*******************************intresting****************
+ 
+// addOne()   // yeha Execute ho jayega but niche bale me nahi 
+function addOne(num) {
+    return num + 1
+
+} 
+ addOne() // return hoga print nahi  print ke liye console
 
 
+ addTwo()  // error aa jayegi    ... Cannot access 'addTwo' before initialization
+ const addTwo = function(num){
+    return num + 2
+ } 
+
+ addTwo() // return hoga print nahi  print ke liye console
