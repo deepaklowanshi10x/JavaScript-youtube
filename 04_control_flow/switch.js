9c8846e6d110f35d00eacff579cb67fc821a8905
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-01-19 17:02:11 +0530

    Switch Statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;               // Switch statement syntax
+
+//     default:
+//         break;
+// }
+
+// control flow using switch statement
+
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;        
+    case 2:
+        console.log("February");
+        break;        
+    case 3:
+        console.log("March");   
+        break;        
+    case 4:
+        console.log("April");   
+        break;        
+
+    default: 
+        console.log("Invalid month");
+        break;
+}
