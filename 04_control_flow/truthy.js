4d85e3180fb05988c62058171299f5b29cc3b63d
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-01-20 13:27:01 +0530

    ternary Opertor

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -36,3 +36,33 @@ else {
  if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
  }
+ 
+
+ // nullish coalescing operator (??) - null and undefined
+
+//?? operator JavaScript me tab use hota hai jab hume kisi value ke null ya undefined hone par ek default value deni ho.
+
+// 👉 Ye sirf null aur undefined ko hi check karta hai
+// 👉 0, false, "" (empty string) ko valid value maanta hai
+// Agar value1 null ya undefined hai → value2 use hogi,, Nahi to → value1 hi milegi
+ 
+let val1;
+val1 = 5 ?? 10;  //5 
+// val1 = null ?? 10; //10
+// val1 = undefined ?? 10; //10
+// val1 = 0 ?? 10;
+// val1 = false ?? true;
+// val1 = "" ?? "default string";
+
+console.log(val1); 
+
+//Ternary Operator (? : )
+
+
+// condition ? true : false 
+// ek line me if else ka kaam kar deta hai
+
+let age = 18;
+// let canVote = (age >= 18) ? "Yes You can Vote" : "No You cannot Vote";  
+// console.log(canVote); 
+age >= 18 ? console.log("Yes You can Vote") : console.log("No You cannot Vote");
