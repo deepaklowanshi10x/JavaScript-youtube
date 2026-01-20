fcab42770411b2b32ceb93605286b1ed3f3469fa
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-01-20 15:28:08 +0530

    iterations in js  like loops

---
+++ 05_iterations/01_for_loop.js
@@ -0,0 +1,50 @@
+// for loops 
+// syntax
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }  
+
+for (let i = 0; i <= 10; i++) { 
+    const element = i;
+    // console.log(element);
+    
+} 
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop: ${i}`); 
+    for (let j = 1; j <= 10; j++) { 
+        // console.log(`   Inner loop: ${j}`); 
+        // console.log(`${i} * ${j} = ${i*j}`);
+        
+    }
+}
+ 
+
+//Array in loop
+const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+console.log(fruits.length);
+for (let i = 0; i < fruits.length; i++) {
+    const fruit = fruits[i];
+    console.log(fruit); 
+    } 
+
+
+//break and continue
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        console.log("Breaking the loop at i = 5");
+        break;  // loop ko yahi rok dega
+    }  
+    console.log(`Value of i: ${i}`);
+}
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        console.log("Breaking the loop at i = 5");
+        continue;  // loop ko yahi skip kar dega aur aage badh jaega
+    }  
+    console.log(`Value of i: ${i}`);
+} 
+
---
+++ 05_iterations/02_while_do_while_loops.js
@@ -0,0 +1,26 @@
+// While looop
+
+let i = 1;
+while (i <= 10) {
+    //console.log(`Loop is : ${i}`);
+    i = i + 1;  // i++
+    } 
+
+    // while loop in array
+
+const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+let j = 0;  
+while (j < fruits.length) {
+    //console.log(fruits[j]);
+   j = j + 1; // j++
+} 
+
+
+// do while loop 
+// ishme pehle ek baar code chalega fir condition check hogi
+
+let score = 1;
+do{
+    console.log(`Score is : ${score}`); 
+    score++;
+} while (score <=10);
